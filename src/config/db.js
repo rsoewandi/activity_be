379f875d457342dec51c91689e49c85f910cac19
rsoewandi <soewandi346@gmail.com>
2026-07-06 23:56:08 +0700

    update config db

--- src/config/db.js
+++ src/config/db.js
@@ -3,12 +3,9 @@ import { Pool } from "pg";
 
 dotenv.config();
 
-const url = process.env.DATABASE_URL || "";
-const wantSsl = /[?&]sslmode=(require|verify-|prefer)/i.test(url);
-
 const pool = new Pool({
-  connectionString: url,
-  ssl: wantSsl ? { rejectUnauthorized: false } : false,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 export default pool;
